d6ecfec8d067bbe64efcd3cdc70cbf9e28fa146e
jeffbrn <jeff.brn@gmail.com>
2024-03-17 19:25:18 -0600

    vector mag

--- matrixlib/src/vector.rs
+++ matrixlib/src/vector.rs
@@ -1,18 +1,26 @@
 use super::Matrix;
 use num_traits::Num;
+use num_traits::ToPrimitive;
 use std::ops::Mul;
 
 pub type Vector<T,const R:usize> = Matrix<T,R,1>;
 
-impl<T: Num+Default+Copy+Mul, const R: usize> Vector<T,R> {
+impl<T: Num+Default+Copy+Mul+ToPrimitive, const R: usize> Vector<T,R> {
     pub fn dot(&self, other: &Vector<T,R>) -> T {
-        if self.size.1 > 1 { panic!("only valid for vectors"); }
         let mut sum = T::default();
         for i in 0..R {
             let x = self.vals[i][0] * other.vals[i][0];
             sum = sum + x;
         }
         sum
+    }
+    pub fn magnitude(&self) -> f64 {
+        let mut sum = T::default();
+        for i in 0..R {
+            sum = sum + self.vals[i][0]*self.vals[i][0];
+        }
+        let x = sum.to_f64().unwrap_or(0.0);
+        x.sqrt()
     }
 }
 impl<T: Num+Default+Copy+Mul> Vector<T,2> {
